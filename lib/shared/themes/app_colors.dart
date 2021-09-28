49e5cbe36817d67362607108c3a6e0725fe89a49
Felipe Ribeiro <piporibeiro@gmail.com>
2021-09-27 21:52:24 -0300

    style: create global themes

---
+++ lib/shared/themes/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final primary = Color(0xFFFF941A);
+  static final grey = Color(0x585666);
+  static final delete = Color(0xE83F5B);
+  static final heading = Color(0x585666);
+  static final body = Color(0x706E7A);
+  static final stroke = Color(0xFFE3E3E6);
+  static final shape = Color(0xFFFAFAFC);
+  static final background = Color(0xFFFFFFFF);
+}
